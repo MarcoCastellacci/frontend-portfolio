0949858293779e527227a31297eac3f22263cb21
Marco Castellacci <marco.casterllacci87@gmail.com>
2022-08-26 12:52:47 -0300

    Create Frontend Repo

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ContactForm from '../Components/Contact-Form'
+
+export default function Contact() {
+
+
+    return (
+        <>
+            <div className="bg-container">
+                <div className="home-container">
+                    <ContactForm />
+                </div>
+            </div>
+        </>
+    )
+}
